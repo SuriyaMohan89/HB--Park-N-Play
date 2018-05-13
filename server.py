96103f9515ea254bfe5fa743a784c0ddff361aea
vagrant <vagrant@vagrant.vm>
2018-05-13 19:26:28 +0000

    Added code for suggestions for nearby park

--- server.py
+++ server.py
@@ -39,16 +39,47 @@ def register_process():
 	password= request.form.get('password')
 	zipcode = int(request.form.get('zipcode'))
 
-	# session['username'] = username
-	# if (User.query.filter(User.username == session[username])):
-	# 	flash("Account already exists. Try Login")
-	# else:
-	user=User(username=username,email=email,password=password,zipcode=zipcode)
-	db.session.add(user)
-	db.session.commit()
-	flash('Successfully Registered')
+	if (User.query.filter(User.username == username) and (User.query.filter(User.email == email))):
+		flash("Account already exists. Try Login")
+	else:
+		user=User(username=username,email=email,password=password,zipcode=zipcode)
+		db.session.add(user)
+		db.session.commit()
+		flash('Successfully Registered')
 	
 	return redirect('/')
+
+
+@app.route('/login',methods=['GET'])
+def login_form():	
+	"""Show Login page"""
+
+	return render_template('login_form.html')
+
+@app.route('/login',methods=['POST'])
+def login_process():
+	"""Process Login form"""
+	login = request.form.get('username')
+	password = request.form.get('password')
+	user = User.query.filter(((User.email == login) | (User.username == login)) & (User.password == password)).first()
+	print user
+	if user:
+		session["user_id"] = user.user_id
+		flash("Successfully Logged In!!")
+		return render_template('user_info.html',user=user)
+
+	else:
+		flash("Incorrect Login details")
+		return redirect('/')
+
+@app.route('/logout')
+def logout():
+	"""Log out the user"""
+	session.pop("user_id",None)
+	flash("You've been logged out!")
+	return redirect('/')
+
+
 
 @app.route('/parks')
 def parks_list():
@@ -62,11 +93,26 @@ def parks_list():
 @app.route('/locatepark')
 def locate_park():
 	""" Locate a park when user types in a zip code """
-
+	zipcode_list = []
 	zipcode = int(request.args.get("zipcode"))
 	locate_park = Park.query.filter(Park.zipcode == zipcode).all()
-
-	return render_template("locate_park.html",locatepark=locate_park)
+	if not locate_park:
+		flash('park not found in zipcode')
+		n=1;
+		while(n<=2):
+			zip_less = zipcode -n;
+			zip_add = zipcode+n;
+			locate_1 = Park.query.filter(Park.zipcode == zip_less).all()
+			if locate_1:
+				zipcode_list.append(locate_1)
+			locate_2= Park.query.filter(Park.zipcode == zip_add).all()
+			if locate_2:
+				zipcode_list.append(locate_2)
+			n+=1
+		print zipcode_list
+		return render_template("locate_park.html",zipcode_list=zipcode_list,locatepark=None)
+	if locate_park:
+		return render_template("locate_park.html",locatepark=locate_park)
 
 
 @app.route('/<int:park_id>' )
